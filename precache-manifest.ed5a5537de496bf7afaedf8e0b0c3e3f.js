666391e234b5d4ab10a1cf96061a7632d0f58207
QiuHaoHao <trouserrr@gmail.com>
2019-10-12 18:47:21 +0800

    Host facial attendance

---
+++ precache-manifest.ed5a5537de496bf7afaedf8e0b0c3e3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f1de3dd2331aa0e9211bed6a88d986",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d9ab658c272c6b39a4d",
+    "url": "/static/css/2.8c467cd4.chunk.css"
+  },
+  {
+    "revision": "7912e6db439be4406d72",
+    "url": "/static/css/main.c24f66c5.chunk.css"
+  },
+  {
+    "revision": "4d9ab658c272c6b39a4d",
+    "url": "/static/js/2.1335a56e.chunk.js"
+  },
+  {
+    "revision": "7912e6db439be4406d72",
+    "url": "/static/js/main.d399bd5e.chunk.js"
+  },
+  {
+    "revision": "547452759540fbc69e57",
+    "url": "/static/js/runtime~main.bd1011f8.js"
+  },
+  {
+    "revision": "bc04722fec2f21fc322f6d0957e63c39",
+    "url": "/static/media/logo.bc04722f.png"
+  }
+]);
